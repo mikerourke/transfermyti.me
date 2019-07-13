9bae3a506042b8c6b53e881e4c2b80575699bf05
Mike Rourke <mike.w.rourke@gmail.com>
2019-07-13 16:27:07 -0700

    fix: ensure Clockify entities are fetched when the review component loads

--- src/containers/entities/reviewClockifyDetailsStep/ReviewClockifyDetailsStep.tsx
+++ src/containers/entities/reviewClockifyDetailsStep/ReviewClockifyDetailsStep.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { If, Then, Else } from "react-if";
 import { css } from "emotion";
@@ -43,71 +43,94 @@ interface ConnectDispatchProps {
 
 type Props = ConnectStateProps & ConnectDispatchProps & StepPageProps;
 
-export const ReviewClockifyDetailsStepComponent: React.FC<Props> = props => {
-  const [isFetching, setIsFetching] = useState<boolean>(true);
+interface State {
+  isFetching: boolean;
+}
 
-  const fetchClockifyEntitiesInAllWorkspaces = async () => {
-    const workspaces = Object.values(props.clockifyWorkspacesById);
+export class ReviewClockifyDetailsStepComponent extends React.Component<
+  Props,
+  State
+> {
+  public constructor(props: Props) {
+    super(props);
+    this.state = {
+      isFetching: true,
+    };
+  }
+
+  public componentDidMount(): void {
+    this.performFetchActions()
+      .then(() => this.setState({ isFetching: false }))
+      .catch(() => this.setState({ isFetching: false }));
+  }
+
+  private performFetchActions = async () => {
+    await this.props.onFetchClockifyWorkspaces();
+    await this.fetchClockifyEntitiesInAllWorkspaces();
+  };
+
+  private fetchClockifyEntitiesInAllWorkspaces = async () => {
+    const workspaces = Object.values(this.props.clockifyWorkspacesById);
     if (workspaces.length === 0) return;
 
     for (const workspace of workspaces) {
-      await props.onFetchClockifyEntitiesInWorkspace(workspace);
+      await this.props.onFetchClockifyEntitiesInWorkspace(workspace);
     }
   };
 
-  useEffect(() => {
-    props
-      .onFetchClockifyWorkspaces()
-      .then(fetchClockifyEntitiesInAllWorkspaces)
-      .then(() => setIsFetching(false))
-      .catch(() => setIsFetching(false));
-  }, []);
+  public render() {
+    const {
+      togglEntitiesByGroupByWorkspace,
+      togglCountsByGroupByWorkspace,
+      togglWorkspacesById,
+    } = this.props;
 
-  return (
-    <If condition={isFetching}>
-      <Then>
-        <Loader>Fetching Clockify entities, please wait...</Loader>
-      </Then>
-      <Else>
-        <EntitiesReviewPage
-          stepNumber={props.stepNumber}
-          subtitle="Review Pending Data Before Transfer"
-          toolName={ToolName.Clockify}
-          entitiesByGroupByWorkspace={props.togglEntitiesByGroupByWorkspace}
-          countsByGroupByWorkspace={props.togglCountsByGroupByWorkspace}
-          workspacesById={props.togglWorkspacesById}
-          onPreviousClick={props.onPreviousClick}
-          onNextClick={props.onNextClick}
-          onRefreshClick={fetchClockifyEntitiesInAllWorkspaces}
-          instructions={
-            <>
-              <p
-                className={css`
-                  margin-bottom: 1rem;
-                `}
-              >
-                This page contains all the records that <strong>will</strong> be
-                created on Clockify. Press <strong>Next</strong> to move to the
-                final step.
-              </p>
-              <p
-                className={css`
-                  color: var(--info);
-                  font-weight: bold;
-                  margin-bottom: 1rem;
-                `}
-              >
-                The transfer will not start until you confirm it on the next
-                page.
-              </p>
-              <InstructionsList />
-            </>
-          }
-        />
-      </Else>
-    </If>
-  );
-};
+    return (
+      <If condition={this.state.isFetching}>
+        <Then>
+          <Loader>Fetching Clockify entities, please wait...</Loader>
+        </Then>
+        <Else>
+          <EntitiesReviewPage
+            stepNumber={this.props.stepNumber}
+            subtitle="Review Pending Data Before Transfer"
+            toolName={ToolName.Clockify}
+            entitiesByGroupByWorkspace={togglEntitiesByGroupByWorkspace}
+            countsByGroupByWorkspace={togglCountsByGroupByWorkspace}
+            workspacesById={togglWorkspacesById}
+            onPreviousClick={this.props.onPreviousClick}
+            onNextClick={this.props.onNextClick}
+            onRefreshClick={this.fetchClockifyEntitiesInAllWorkspaces}
+            instructions={
+              <>
+                <p
+                  className={css`
+                    margin-bottom: 1rem;
+                  `}
+                >
+                  This page contains all the records that <strong>will</strong>{" "}
+                  be created on Clockify. Press <strong>Next</strong> to move to
+                  the final step.
+                </p>
+                <p
+                  className={css`
+                    color: var(--info);
+                    font-weight: bold;
+                    margin-bottom: 1rem;
+                  `}
+                >
+                  The transfer will not start until you confirm it on the next
+                  page.
+                </p>
+                <InstructionsList />
+              </>
+            }
+          />
+        </Else>
+      </If>
+    );
+  }
+}
 
 const mapStateToProps = (state: ReduxState) => ({
   clockifyWorkspacesById: selectClockifyIncludedWorkspacesById(state),
