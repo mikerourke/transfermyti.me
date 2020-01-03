ac9fd4c1af0b44018f968328a42b675c6a6b5700
Mike Rourke <mike.w.rourke@gmail.com>
2020-01-02 17:56:49 -0800

    feat: add initial Modal component, add sizes to Toggle, and remove unused components

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,79 +1,95 @@
 import React from "react";
 import { styled } from "./emotion";
 
-const Button = styled.button<{ isToggled: boolean }>(
+type Size = "large" | "small";
+
+const height = (size: Size): string => (size === "small" ? "1.5rem" : "2rem");
+
+const width = (size: Size): string => (size === "small" ? "3.5rem" : "4.5rem");
+
+const scale = (value: string, percentage: number): string => {
+  const numericValue = parseFloat(value.replace("rem", ""));
+  return (numericValue * percentage).toFixed(3).concat("rem");
+};
+
+const Button = styled.button<{ isToggled: boolean; size: Size }>(
   {
     border: 0,
-    borderRadius: "2rem",
-    clear: "both",
     display: "inline-block",
-    float: "left",
-    minHeight: "2rem",
     overflow: "hidden",
     position: "relative",
     transition: "background 0.1s ease-in-out",
-    width: "4.5rem",
   },
-  ({ isToggled, theme }) => ({
+  ({ isToggled, size, theme }) => ({
     background: isToggled ? theme.colors.cornflower : theme.colors.alto,
+    borderRadius: height(size),
+    height: height(size),
+    width: width(size),
   }),
 );
 
-const Span = styled.span<{ isToggled: boolean }>(
+const Span = styled.span<{ isToggled: boolean; size: Size }>(
   {
     background: "white",
     borderRadius: "50%",
     display: "inline-block",
-    fontSize: "0.875rem",
     fontWeight: "bold",
-    height: "1.625rem",
     pointerEvents: "none",
     position: "absolute",
     textTransform: "uppercase",
-    top: "0.175rem",
     transition: "0.4s",
-    width: "1.625rem",
 
     "&::before,&::after": {
       display: "block",
       position: "absolute",
-      top: "0.325rem",
     },
   },
-  ({ isToggled, theme }) => ({
+  ({ isToggled, size, theme }) => ({
     border: `1px solid ${theme.colors.alto}`,
-    left: isToggled ? "2.625rem" : "0.175rem",
+    fontSize: scale(height(size), 0.5),
+    left: isToggled ? scale(width(size), 0.6125) : scale(width(size), 0.05),
+    top: scale(height(size), 0.1),
+    height: scale(height(size), 0.8),
+    width: scale(height(size), 0.8),
 
     "&::before,&::after": {
       color: isToggled ? theme.colors.white : theme.colors.midnight,
+      lineHeight: scale(height(size), 0.8),
     },
 
     "&::before": {
       content: isToggled ? `"YES"` : undefined,
-      left: isToggled ? "-2rem" : undefined,
+      left: isToggled ? scale(width(size), -0.4825) : undefined,
     },
 
     "&::after": {
       content: isToggled ? undefined : `"NO"`,
-      right: isToggled ? undefined : "-2.125rem",
+      right: isToggled ? undefined : scale(width(size), -0.475),
     },
   }),
 );
 
 interface Props {
   isToggled: boolean;
+  size?: Size;
   onToggle: VoidFunction;
 }
 
-const Toggle: React.FC<Props> = ({ isToggled, onToggle, ...props }) => (
+const Toggle: React.FC<Props> = ({
+  isToggled,
+  size = "large",
+  onToggle,
+  ...props
+}) => (
   <Button
     aria-checked={isToggled}
     isToggled={isToggled}
     role="switch"
+    size={size}
     onClick={onToggle}
     {...props}
   >
-    <Span isToggled={isToggled} />
+    <Span isToggled={isToggled} size={size} />
   </Button>
 );
 
