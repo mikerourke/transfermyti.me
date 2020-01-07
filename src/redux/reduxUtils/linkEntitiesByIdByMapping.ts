a47dbb4eae5c4563690b34066ecdf1d845e09c16
Mike Rourke <mike.w.rourke@gmail.com>
2020-01-06 18:08:31 -0800

    refactor: move linking code for time entries to `linkEntitiesByIdByMapping` file

--- src/redux/reduxUtils/linkEntitiesByIdByMapping.ts
+++ src/redux/reduxUtils/linkEntitiesByIdByMapping.ts
@@ -1,6 +1,36 @@
+import differenceInMinutes from "date-fns/differenceInMinutes";
 import * as R from "ramda";
-import { EntityGroup, Mapping } from "~/allEntities/allEntitiesTypes";
+import {
+  BaseEntityModel,
+  EntityGroup,
+  Mapping,
+} from "~/allEntities/allEntitiesTypes";
+import {
+  TimeEntryModel,
+  TimeEntriesByIdModel,
+} from "~/timeEntries/timeEntriesTypes";
 
+/**
+ * Sets the `linkedId` and `isIncluded` field of the source and target records
+ * and returns an object with a `source` and `target` property containing the
+ * corresponding records by their `id` field.
+ * @example
+ *   const sourceRecords = [{ id: "S1", name: "Foo" }, { id: "S2", name: "Bar" }];
+ *   const targetRecords = [{ id: "T1", name: "Foo" }, { id: "T2", name: "Baz" }];
+ *
+ *   const result = linkEntitiesByIdByMapping(sourceRecords, targetRecords);
+ *   console.log(result);
+ *   {
+ *     source: {
+ *       S1: { id: "S1", name: "Foo", linkedId: "T1", isIncluded: false },
+ *       S2: { id: "S2", name: "Bar", linkedId: null, isIncluded: true },
+ *     },
+ *     target: {
+ *       T1: { id: "T1", name: "Foo", linkedId: "S1", isIncluded: false },
+ *       T2: { id: "T2", name: "Bar", linkedId: null, isIncluded: true },
+ *     },
+ *   }
+ */
 export function linkEntitiesByIdByMapping<TEntity>(
   sourceRecords: TEntity[],
   targetRecords: TEntity[],
@@ -12,12 +42,27 @@ export function linkEntitiesByIdByMapping<TEntity>(
     };
   }
 
+  const [{ memberOf }] = (sourceRecords as unknown) as BaseEntityModel[];
+  if (memberOf === EntityGroup.TimeEntries) {
+    // TypeScript freaks out on this one, but I don't want to add 400 type
+    // assertions. I know they're time entries and I know the function is going
+    // to return the correct values!
+    // @ts-ignore
+    return linkForMappingForTimeEntries(sourceRecords, targetRecords);
+  }
+
   return {
     source: linkForMappingByName(targetRecords, sourceRecords),
     target: linkForMappingByName(sourceRecords, targetRecords),
   };
 }
 
+/**
+ * Links the corresponding records by their `name` property. If the `name`
+ * for the source and target records match, set the `linkedId` for the source
+ * to the target ID and vice versa. Also, if a match is found, set `isIncluded`
+ * to `false` to prevent duplicate records/API errors.
+ */
 function linkForMappingByName<TEntity>(
   linkFromRecords: TEntity[],
   recordsToUpdate: TEntity[],
@@ -28,10 +73,14 @@ function linkForMappingByName<TEntity>(
     memberOf: EntityGroup;
   };
 
-  const linkFromEntitiesByName = {};
-  for (const linkFromRecord of linkFromRecords as LinkableRecord[]) {
-    linkFromEntitiesByName[linkFromRecord.name] = linkFromRecord;
-  }
+  const linkFromEntitiesByName = R.indexBy<LinkableRecord>(
+    R.prop("name"),
+    linkFromRecords as LinkableRecord[],
+  );
+
+  // for (const linkFromRecord of linkFromRecords as LinkableRecord[]) {
+  //   linkFromEntitiesByName[linkFromRecord.name] = linkFromRecord;
+  // }
 
   const linkedRecordsById = {};
   for (const recordToUpdate of recordsToUpdate as LinkableRecord[]) {
@@ -52,4 +101,63 @@ function linkForMappingByName<TEntity>(
   }
 
   return linkedRecordsById;
+}
+
+/**
+ * Compares the source and target time entries and sets the `linkedId` for
+ * matching entries. If an entries' match is found, set the `isIncluded`
+ * property to false. The criteria for what represents a "matching" entry is
+ * in the `doTimeEntriesMatch()` function below.
+ */
+function linkForMappingForTimeEntries(
+  sourceTimeEntries: TimeEntryModel[],
+  targetTimeEntries: TimeEntryModel[],
+): Record<Mapping, TimeEntriesByIdModel> {
+  // Expedite the matching process by sorting by start date (since the start
+  // and end date/time are compared against each other):
+  const sortByDate = R.sortBy(R.prop("start"));
+  const sortedSourceEntries = sortByDate(sourceTimeEntries);
+  const sortedTargetEntries = sortByDate(targetTimeEntries);
+
+  const sourceById: Record<string, TimeEntryModel> = {};
+  const targetById: Record<string, TimeEntryModel> = {};
+
+  for (const sourceEntry of sortedSourceEntries) {
+    sourceById[sourceEntry.id] = sourceEntry;
+
+    for (const targetEntry of sortedTargetEntries) {
+      targetById[targetEntry.id] = targetEntry;
+
+      if (doTimeEntriesMatch(sourceEntry, targetEntry)) {
+        sourceById[sourceEntry.id].linkedId = targetEntry.id;
+        targetById[targetEntry.id].linkedId = sourceEntry.id;
+      }
+    }
+
+    sourceById[sourceEntry.id].isIncluded = R.isNil(
+      sourceById[sourceEntry.id].linkedId,
+    );
+  }
+
+  return {
+    source: sourceById,
+    target: targetById,
+  };
+}
+
+/**
+ * Compares the two time entries and returns true if they _appear_ to match
+ * based on the date, description, flags, etc.
+ */
+function doTimeEntriesMatch(
+  sourceEntry: TimeEntryModel,
+  targetEntry: TimeEntryModel,
+): boolean {
+  return [
+    sourceEntry.description === targetEntry.description,
+    sourceEntry.isActive === targetEntry.isActive,
+    sourceEntry.isBillable === targetEntry.isBillable,
+    differenceInMinutes(sourceEntry.start, targetEntry.start) <= 1,
+    differenceInMinutes(sourceEntry.end, targetEntry.end) <= 1,
+  ].every(Boolean);
 }
--- src/timeEntries/sagas/timeEntriesSagas.ts
+++ src/timeEntries/sagas/timeEntriesSagas.ts
@@ -1,7 +1,6 @@
 import { SagaIterator } from "@redux-saga/types";
 import { call, put, select } from "redux-saga/effects";
-import differenceInMinutes from "date-fns/differenceInMinutes";
-import * as R from "ramda";
+import { linkEntitiesByIdByMapping } from "~/redux/reduxUtils";
 import { showFetchErrorNotification } from "~/app/appActions";
 import { toolNameByMappingSelector } from "~/app/appSelectors";
 import {
@@ -17,11 +16,8 @@ import {
   createTogglTimeEntriesSaga,
   fetchTogglTimeEntriesSaga,
 } from "./togglTimeEntriesSagas";
-import { Mapping, ToolName } from "~/allEntities/allEntitiesTypes";
-import {
-  TimeEntryModel,
-  TimeEntriesByIdModel,
-} from "~/timeEntries/timeEntriesTypes";
+import { ToolName } from "~/allEntities/allEntitiesTypes";
+import { TimeEntryModel } from "~/timeEntries/timeEntriesTypes";
 
 export function* createTimeEntriesSaga(): SagaIterator {
   yield put(createTimeEntries.request());
@@ -41,7 +37,7 @@ export function* createTimeEntriesSaga(): SagaIterator {
       sourceTimeEntries,
     );
 
-    const timeEntriesByIdByMapping = linkTimeEntriesByIdByMapping(
+    const timeEntriesByIdByMapping = linkEntitiesByIdByMapping<TimeEntryModel>(
       sourceTimeEntries,
       targetTimeEntries,
     );
@@ -68,7 +64,7 @@ export function* fetchTimeEntriesSaga(): SagaIterator {
     sourceTimeEntries = sortTimeEntries(sourceTimeEntries);
     targetTimeEntries = sortTimeEntries(targetTimeEntries);
 
-    const timeEntriesByIdByMapping = linkTimeEntriesByIdByMapping(
+    const timeEntriesByIdByMapping = linkEntitiesByIdByMapping<TimeEntryModel>(
       sourceTimeEntries,
       targetTimeEntries,
     );
@@ -82,68 +78,4 @@ export function* fetchTimeEntriesSaga(): SagaIterator {
 
 function sortTimeEntries(timeEntries: TimeEntryModel[]): TimeEntryModel[] {
   return timeEntries.sort((a, b) => a.start.getTime() - b.start.getTime());
-}
-
-function linkTimeEntriesByIdByMapping(
-  sourceTimeEntries: TimeEntryModel[],
-  targetTimeEntries: TimeEntryModel[],
-): Record<Mapping, TimeEntriesByIdModel> {
-  if (sourceTimeEntries.length === 0) {
-    return {
-      source: {},
-      target: {},
-    };
-  }
-
-  const sortByDate = R.sortBy(R.prop("start"));
-  const sortedSourceEntries = sortByDate(sourceTimeEntries);
-  const sortedTargetEntries = sortByDate(targetTimeEntries);
-
-  const sourceById: TimeEntriesByIdModel = {};
-  const targetById: TimeEntriesByIdModel = {};
-
-  for (const sourceEntry of sortedSourceEntries) {
-    sourceById[sourceEntry.id] = sourceEntry;
-
-    for (const targetEntry of sortedTargetEntries) {
-      targetById[targetEntry.id] = targetEntry;
-
-      // TODO: Make sure this actually works!
-      if (doTimeEntriesMatch(sourceEntry, targetEntry)) {
-        sourceById[sourceEntry.id].linkedId = targetEntry.id;
-        targetById[targetEntry.id].linkedId = sourceEntry.id;
-      }
-    }
-
-    sourceById[sourceEntry.id].isIncluded = R.isNil(
-      sourceById[sourceEntry.id].linkedId,
-    );
-  }
-
-  return {
-    source: sourceById,
-    target: targetById,
-  };
-}
-
-/**
- * Compares the two time entries and returns true if they _appear_ to match
- * based on the date, description, flags, etc.
- */
-function doTimeEntriesMatch(
-  sourceEntry: TimeEntryModel,
-  targetEntry: TimeEntryModel,
-): boolean {
-  if (
-    [
-      sourceEntry.description !== targetEntry.description,
-      sourceEntry.year !== targetEntry.year,
-      sourceEntry.isActive !== targetEntry.isActive,
-      sourceEntry.isBillable !== targetEntry.isBillable,
-    ].some(Boolean)
-  ) {
-    return false;
-  }
-
-  return differenceInMinutes(sourceEntry.start, targetEntry.start) <= 1;
 }
