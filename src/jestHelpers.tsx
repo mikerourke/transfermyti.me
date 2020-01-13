211b2fcb71d38f34b2a6f607c41da6048d5329f0
Mike Rourke <mike.w.rourke@gmail.com>
2020-01-12 20:23:06 -0800

    test: add helper functions for running Jest tests

---
+++ src/jestHelpers.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies,@typescript-eslint/no-explicit-any,no-console */
+import React from "react";
+import {
+  RenderOptions,
+  RenderResult,
+  render as testRender,
+} from "@testing-library/react";
+import { ThemeProvider } from "emotion-theming";
+import { theme } from "./components/emotion";
+
+// These are exported for convenience (so we don't have to add an additional import
+// to the test file).
+export { createEvent, fireEvent } from "@testing-library/react";
+
+/**
+ * Renders the specified component with react-testing-library wrapped in a theme
+ * provider so we don't have to do it explicitly for each component.
+ */
+export function render(
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, "queries">,
+): RenderResult {
+  return testRender(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, options);
+}
+
+type LogMethod = "log" | "warn" | "error";
+
+export class ConsoleToggle {
+  private consoleMethod: (...args: any[]) => void = console.warn;
+  private readonly logMethod: LogMethod;
+
+  public constructor(logMethod: LogMethod) {
+    this.logMethod = logMethod;
+  }
+
+  public onBefore(): void {
+    this.consoleMethod = console[this.logMethod];
+    console[this.logMethod] = jest.fn();
+  }
+
+  public onAfter(): void {
+    console[this.logMethod] = this.consoleMethod;
+  }
+}
