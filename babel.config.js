f91d5ff4f3c46d104d73cbbca5a8f27984c09309
Mike Rourke <mike.w.rourke@gmail.com>
2022-03-21 13:26:49 -0700

    chore: rename Babel configuration file

---
+++ babel.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "^~/(.+)": "./src/\\1",
+        },
+      },
+    ],
+    [
+      "transform-inline-environment-variables",
+      {
+        exclude: ["USE_LOCAL_API", "PARCEL_SERVE"],
+      },
+    ],
+    [
+      "jsx-pragmatic",
+      {
+        export: "jsx",
+        module: "@emotion/react",
+        import: "___EmotionJSX",
+      },
+    ],
+    [
+      "@babel/plugin-transform-react-jsx",
+      {
+        pragma: "___EmotionJSX",
+        pragmaFrag: "React.Fragment",
+      },
+    ],
+    [
+      "@emotion/babel-plugin",
+      {
+        autoLabel: "dev-only",
+        labelFormat: "[local]",
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-proposal-numeric-separator"],
+    },
+  },
+};
