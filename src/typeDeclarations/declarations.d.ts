dc34def4498c062165a59b791ad776f2fbdedb0c
Mike Rourke <mike.w.rourke@gmail.com>
2022-03-27 11:08:30 -0700

    refactor: create `typeDeclarations` directory and spilt up types

---
+++ src/typeDeclarations/declarations.d.ts
@@ -0,0 +1,19 @@
+// Used to avoid the no-explicit-any ESLint rule explicitly. In some cases, it
+// is totally OK to just say it's `any`:
+declare type AnyValid = any;
+
+declare type Dictionary<T> = Record<string, T>;
+
+declare interface Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__<R>(
+    options?: ReduxDevToolsComposeOptions,
+  ): (...args: AnyValid[]) => R;
+}
+
+declare type InputChangeEvent = Event & {
+  currentTarget: EventTarget & HTMLInputElement;
+};
+
+declare type SelectChangeEvent = Event & {
+  currentTarget: EventTarget & HTMLSelectElement;
+};
